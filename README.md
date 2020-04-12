52efe344726a2de2cc0b600cd72c60e9ee91a8c5
Galaxyzeta <1919737818@qq.com>
2020-04-12 23:17:05 +0800

    改成一遍读取

---
+++ README.md
@@ -0,0 +1,19 @@
+# HDU编译原理实验
+Compiler Principle Homework in Hangzhou Dianzi University.
+
+作者：@Galaxyzeta
+
+## 基于java的词法分析器
+A Java-based Lexual Analyzer.
+
+特点：一遍扫描，准确分析。使用反射特性，轻松添加分析规则。
+
+过程概述：使用 `FileReader` 类从文本文件逐字符读取程序，直到每读到`EOF`。每一个字符存储在`StringBuilder`缓冲区，每次读取从缓冲区取出字符串进行 **正则表达式** 匹配，并对 **超读** 现象进行暂停1轮文件读取的操作。结果保存为2元式，写入文本文件。
+
+类概述：
+
+1. `PatternFactory`：在其中添加匹配规则。包含一个用反射设计的匹配方法，参数1为匹配规则字符串，2为 
+2. `WordAnalyzer`：词法分析类。包含一个边读取边分析的方法，以及根据对应匹配规则名称确定编码类别的 `HashMap`。
+3. `WordCodec`：编码类别枚举。
+4. `ParseResult`：生成的二元式实体类。
+
--- com/galaxyzeta/WordAnalyzer.java
+++ com/galaxyzeta/WordAnalyzer.java
@@ -26,7 +26,7 @@ public class WordAnalyzer {
 	
 	private static HashMap<String, Integer> codecMap = new HashMap<>();
 	private static HashMap<String, Integer> fix = new HashMap<>();
-	private static LinkedList<Character> blanklist = new LinkedList<>(Arrays.asList(' ', '\t', '\n'));
+	private static LinkedList<Character> blanklist = new LinkedList<>(Arrays.asList(' ', '\t', '\n', (char)13));
 
 	static {
 		// Put fore-read fix into hashmap
@@ -61,15 +61,79 @@ public class WordAnalyzer {
 	 */
 	public void readProgram() {
 		StringBuilder sb = new StringBuilder();
+		boolean blankFlag = false;
 		try (FileReader fr = new FileReader(inputPath)) {
-			int c;
-			while((c = fr.read()) != -1) {
-				if (c==13) {
-					sb.append('\n');
+			int c = -1;
+			int blocked = 0;
+			boolean quitFlag = false;
+			while(quitFlag == false) {
+				//EOF detect
+				if(blocked == 0) {
+					c = fr.read();
 				} else {
-					sb.append((char)c);
+					blocked -= 1;
+				}
+				if(c == -1) {
+					quitFlag = true;
+				}
+				char chr = (char)c;
+				if(c == 13 || c == -1) {
+					chr = '\n';
+				}
+				// Judge useless blank to save time.
+				if (blanklist.contains(chr)) {
+					if (blankFlag == true) {
+						continue;
+					} else {
+						blankFlag = true;
+					}
+				} else {
+					blankFlag = false;
+				}
+
+				// Read a character to buffer zone.
+				sb.append(chr);
+				String str = sb.toString();
+
+				// Iterate over a priority base array. Try to fit into each pattern.
+				ArrayList<String> arr = new ArrayList<>(Arrays.asList("KEYWORD", "ID", "NUMBER", "OPERATOR_TYPE1","OPERATOR_TYPE2", "COMMENT", "PARENTHESE", "EOL", "BLANK"));
+				for (String each : arr) {
+					String match = PatternFactory.checkPattern(each, str);
+					if (match != null) {
+						// Handle fore read.
+						if (fix.containsKey(each)) {
+							blocked = 1;
+							match = match.substring(0, match.length() + fix.get(each));
+						}
+						// Clean up buffer zone.
+						sb = new StringBuilder();
+						// Show debug message if VERBOSE == true
+						if (verbose == true) {
+							System.out.println(String.format("[INFO]Detected %s , Mode = %s", match, each));
+						}
+						// Save result into a map. Only useful results can be saved.
+						int codec = codecMap.get(each);
+						if (codec>=0) {
+							tupleResult.add(new ParseResult(match, codec));
+						}
+						break;
+					}
 				}
 			}
+			System.out.println(tupleResult);
+
+			// Write result into a file
+			try (FileWriter fw = new FileWriter(new File(outputPath))){
+				sb = new StringBuilder();
+				for (ParseResult pr : tupleResult) {
+					sb.append(pr.toString());
+					sb.append('\n');
+				}
+				fw.write(sb.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 			sb.append('\n');
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -84,76 +148,11 @@ public class WordAnalyzer {
 		this.program = program;
 	}
 
-	/**
-	 * Analyze program word patterns from variable <i>program</i>
-	 */
-	public void analyzeMain() {
-		StringBuilder buff = new StringBuilder();
-		int strlen = program.length();
-		char c;
-		boolean blankFlag = true;
-		
-		for (int i = 0; i < strlen; i++) {
-			c = program.charAt(i);
-			// Judge useless blank to save time.
-			if (blanklist.contains(c)) {
-				if (blankFlag == true) {
-					continue;
-				} else {
-					blankFlag = true;
-				}
-			} else {
-				blankFlag = false;
-			}
-			// Read a character to buffer zone.
-			buff.append(c);
-			String str = buff.toString();
-
-			// Iterate over a priority base array. Try to fit into each pattern.
-			ArrayList<String> arr = new ArrayList<>(Arrays.asList("KEYWORD", "ID", "NUMBER", "OPERATOR_TYPE1","OPERATOR_TYPE2", "COMMENT", "PARENTHESE", "EOL", "BLANK"));
-			for (String each : arr) {
-				String match = PatternFactory.checkPattern(each, str);
-				if (match != null) {
-					// Handle fore read.
-					if (fix.containsKey(each)) {
-						i += fix.get(each);
-						match = match.substring(0, match.length() + fix.get(each));
-					}
-					// Clean up buffer zone.
-					buff = new StringBuilder();
-					// Show debug message if VERBOSE == true
-					if (verbose == true) {
-						System.out.println(String.format("[INFO]Detected %s , Mode = %s", match, each));
-					}
-					// Save result into a map. Only useful results can be saved.
-					int codec = codecMap.get(each);
-					if (codec>=0) {
-						tupleResult.add(new ParseResult(match, codec));
-					}
-					break;
-				}
-			}
-		}
-		System.out.println(tupleResult);
-		// Write result into a file
-		try (FileWriter fw = new FileWriter(new File(outputPath))){
-			StringBuilder sb = new StringBuilder();
-			for (ParseResult pr : tupleResult) {
-				sb.append(pr.toString());
-				sb.append('\n');
-			}
-			fw.write(sb.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public static void main(String[] args) {
 		System.out.println((int)'\n');
 		//String s = "+-*/123+++++--- asdbegin begin if (;)#if else then\nasd";
 		WordAnalyzer wa = new WordAnalyzer("other/input.txt", "other/log.txt");
 		wa.readProgram();
 		//wa.readProgramDebug(")\na:=333");
-		wa.analyzeMain();
 	}
 }
